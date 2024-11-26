c7b4e3be6f39e27a1fbaa2b6673b3e392b669d39
marcos <marcgabe@myuax.com>
2024-11-26 19:34:23 +0100

    Initial commit

---
+++ src/main/java/org/example/backendwakandaresiduos/BackendWakandaResiduosApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaresiduos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaResiduosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaResiduosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendwakandaresiduos/BackendWakandaResiduosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaresiduos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaResiduosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
